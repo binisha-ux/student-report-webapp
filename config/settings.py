806fe2fad827061491680406b9b9619bab06118a
binisha-ux <beenesha00@gmail.com>
2026-09-04 13:27:51 +0545

    Configure PostgreSQL and apply initial report_card migrations

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'report_card',
 ]
 
 MIDDLEWARE = [
@@ -74,8 +75,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'reportcard_db',      
+        'USER': 'postgres',     
+        'PASSWORD': 'admin',    
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
